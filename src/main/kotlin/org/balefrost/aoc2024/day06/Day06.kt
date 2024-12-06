d4365ec59a0dbb6330ec75882c8c60b27808a0cc
Dan Yankowsky <bytecrafter@gmail.com>
2024-12-05 21:41:38 -0800

    day06 part1 part2

---
+++ src/main/kotlin/org/balefrost/aoc2024/day06/Day06.kt
@@ -0,0 +1,91 @@
+package org.balefrost.aoc2024.day06
+
+import org.balefrost.aoc2024.readInputLines
+
+data class XY(val x: Int, val y: Int) {
+    fun turnRight(): XY {
+        return XY(-y, x)
+    }
+
+    operator fun plus(other: XY): XY {
+        return XY(x + other.x, y + other.y)
+    }
+}
+
+data class PosDir(val pos: XY, val dir: XY) {
+    fun turnRight(): PosDir {
+        return PosDir(pos, dir.turnRight())
+    }
+
+    fun step(): PosDir {
+        return PosDir(pos + dir, dir)
+    }
+}
+
+fun findStartingPos(map: List<String>): XY {
+    for (y in map.indices) {
+        for (x in map[y].indices) {
+            if (map[y][x] == '^') {
+                return XY(x, y)
+            }
+        }
+    }
+    throw IllegalArgumentException()
+}
+
+enum class WalkTermination { OffEdge, Loop }
+data class WalkResult(val path: List<PosDir>, val termination: WalkTermination)
+
+fun doWalk(initialState: PosDir, map: List<String>): WalkResult {
+    var currentState = initialState
+    val visitedStates = mutableSetOf<PosDir>()
+    fun inBounds(xy: XY): Boolean = xy.y in map.indices && xy.x in map[0].indices
+    while (inBounds(currentState.pos)) {
+        if (!visitedStates.add(currentState)) {
+            return WalkResult(visitedStates.toList(), WalkTermination.Loop)
+        }
+        val newState = currentState.step()
+        if (!inBounds(newState.pos)) {
+            break
+        }
+        if (map[newState.pos.y][newState.pos.x] == '#') {
+            currentState = currentState.turnRight()
+        } else {
+            currentState = newState
+        }
+    }
+    return WalkResult(visitedStates.toList(), WalkTermination.OffEdge)
+}
+
+object Day06Part01 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val map = readInputLines("inputs/day06.txt")
+        val walkResult = doWalk(PosDir(findStartingPos(map), XY(0, -1)), map)
+        println(walkResult.path.map { it.pos }.distinct().size)
+    }
+}
+
+fun setObstruction(map: List<String>, pos: XY): List<String> {
+    return map.mapIndexed { index, s ->
+        if (index == pos.y) {
+            s.replaceRange(pos.x .. pos.x, "#")
+        } else {
+            s
+        }
+    }
+}
+
+object Day06Part02 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val map = readInputLines("inputs/day06.txt")
+        val initialState = PosDir(findStartingPos(map), XY(0, -1))
+        val pristineWalkResult = doWalk(initialState, map)
+        val loops = pristineWalkResult.path.map { it.pos }.distinct().count { candidate ->
+            val experimentWalkResult = doWalk(initialState, setObstruction(map, candidate))
+            experimentWalkResult.termination == WalkTermination.Loop
+        }
+        println(loops)
+    }
+}
