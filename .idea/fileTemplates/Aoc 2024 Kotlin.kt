ef956ef43c1ec209aebbb4b5085f5d7bcf35d0df
Dan Yankowsky <bytecrafter@gmail.com>
2024-12-09 22:08:54 -0800

    Store file template in VCS

---
+++ .idea/fileTemplates/Aoc 2024 Kotlin.kt
@@ -0,0 +1,17 @@
+package org.balefrost.aoc2024.day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}
+
+import org.balefrost.aoc2024.readInputLines
+
+object Day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}Part01 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = readInputLines("inputs/day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}.txt")
+    }
+}
+
+object Day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}Part02 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = readInputLines("inputs/day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}.txt")
+    }
+}
