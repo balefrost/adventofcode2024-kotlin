604aa8244022314afe523ad2748498a9aae81fbe
Dan Yankowsky <bytecrafter@gmail.com>
2024-12-12 00:18:01 -0800

    visualization day12 part1

--- src/main/kotlin/org/balefrost/aoc2024/day12/Day12.kt
+++ src/main/kotlin/org/balefrost/aoc2024/day12/Day12.kt
@@ -3,10 +3,11 @@ package org.balefrost.aoc2024.day12
 import org.balefrost.aoc2024.StringBased2DMap
 import org.balefrost.aoc2024.XY
 import org.balefrost.aoc2024.readInputLines
-import java.awt.*
+import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.GridLayout
 import javax.swing.*
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
 
 data class Plot(val type: Char, val items: Set<XY>)
 
@@ -91,5 +92,129 @@ object Day12Part02 {
         }
 
         println(cost)
+    }
+}
+
+suspend fun <R, T> SequenceScope<T>.srun(block: suspend SequenceScope<T>.() -> R): R {
+    return block()
+}
+
+object Day12Part01Vis {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = readInputLines("inputs/day12.txt")
+
+        val grid = StringBased2DMap(lines, '.')
+        val plots = findPlots(grid)
+
+        val plotAdjacency = plots.associateWith { plot ->
+            val allAdjacent =
+                plot.items.asSequence().flatMap { it.adjacent }.distinct().filterNot { it in plot.items }.toSet()
+            plots.filter { it != plot && it.items.intersect(allAdjacent).isNotEmpty() }
+        }
+
+        var numColors = 0
+        val plotColorIndices = mutableMapOf<Plot, Int>()
+        for (plot in plots) {
+            val adjacentPlots = plotAdjacency.getValue(plot)
+            val adjacentColorIndices = adjacentPlots.mapNotNull(plotColorIndices::get).toSet()
+            var colorToUse = (0..<numColors).firstOrNull { it !in adjacentColorIndices }
+            if (colorToUse == null) {
+                colorToUse = numColors++
+            }
+            plotColorIndices[plot] = colorToUse
+        }
+
+        val colors = (0..<numColors).map { Color.getHSBColor(it.toFloat() / numColors, 1.0f, 1.0f) }.toTypedArray()
+
+        val positionToColor = plotColorIndices.flatMap { (plot, idx) ->
+            plot.items.map {
+                it to colors[idx]
+            }
+        }.toMap()
+
+        data class GraphicsOp(val pos: XY, val op: (JLabel) -> Unit)
+
+        val steps = sequence {
+            val finished = mutableSetOf<XY>()
+            var previousTrailhead: XY? = null
+
+            for (position in grid.positions) {
+                if (!finished.add(position)) {
+                    continue
+                }
+                val saturatedColor = positionToColor.getValue(position)
+                val dimColor = saturatedColor.darker()
+
+                srun {
+                    val changeOps = mutableListOf<GraphicsOp>()
+                    if (previousTrailhead != null) {
+                        changeOps += GraphicsOp(previousTrailhead!!) {
+                            it.border = BorderFactory.createLineBorder(Color(0xff, 0xff, 0xff, 0x00), 1)
+                        }
+                    }
+                    changeOps += GraphicsOp(position) {
+                        it.border = BorderFactory.createLineBorder(Color.BLACK, 1)
+                    }
+                    yield(changeOps)
+                }
+
+                val type = grid[position]
+                val plot = mutableSetOf<XY>()
+                var more = setOf(position)
+                while (more.isNotEmpty()) {
+                    yield(more.map { xy ->
+                        GraphicsOp(xy) {
+                            it.background = dimColor
+                        }
+                    })
+                    plot += more
+                    more = more.asSequence().flatMap { m -> m.adjacent }.distinct()
+                        .filter { it in grid && it !in plot && grid[it] == type }.toSet()
+                }
+                yield(plot.map { xy ->
+                    GraphicsOp(xy) { it.background = saturatedColor }
+                })
+                finished.addAll(plot)
+
+                previousTrailhead = position
+            }
+        }
+
+        val stepsIter = steps.iterator()
+        val f = JDialog()
+        val labels = mutableMapOf<XY, JLabel>()
+        fun takeStep() {
+            if (stepsIter.hasNext()) {
+                val ops = stepsIter.next()
+                for (op in ops) {
+                    labels[op.pos]?.let { op.op(it) }
+                }
+            }
+        }
+        f.contentPane.layout = BorderLayout()
+        val gridPanel = JPanel()
+        val gridScroll =
+            JScrollPane(gridPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS)
+        f.contentPane.add(gridScroll, BorderLayout.CENTER)
+        gridPanel.layout = GridLayout(grid.dims.w, grid.dims.h)
+        for (y in 0..<grid.dims.h) {
+            for (x in 0..<grid.dims.w) {
+                val pos = XY(x, y)
+                val l = JLabel("${grid[pos]}").apply {
+                    isOpaque = true
+                    maximumSize = Dimension(10, 10)
+                    minimumSize = Dimension(10, 10)
+                    size = Dimension(10, 10)
+                    border = BorderFactory.createLineBorder(Color(0xff, 0xff, 0xff, 0x00), 1)
+                }
+                labels[pos] = l
+                gridPanel.add(l)
+            }
+        }
+        Timer(2) { takeStep() }.start()
+        f.pack()
+        f.defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+        f.isVisible = true
     }
 }
