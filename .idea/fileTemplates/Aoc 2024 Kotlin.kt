cc4e37e181a2a2917648893e06e49e1693818817
Dan Yankowsky <bytecrafter@gmail.com>
2024-12-17 21:28:33 -0800

    Update file template

--- .idea/fileTemplates/Aoc 2024 Kotlin.kt
+++ .idea/fileTemplates/Aoc 2024 Kotlin.kt
@@ -5,13 +5,15 @@ import org.balefrost.aoc2024.readInputLines
 object Day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}Part01 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val lines = readInputLines("inputs/day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}.txt")
-    }
+//        val lines = readInputLines("inputs/day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}.txt")
+        val lines = """
+            """.trimIndent().lines()    }
 }
 
 object Day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}Part02 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val lines = readInputLines("inputs/day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}.txt")
-    }
+//        val lines = readInputLines("inputs/day${DAY_NUMBER_PLEASE_PAD_TO_TWO_DIGITS}.txt")
+        val lines = """
+            """.trimIndent().lines()    }
 }
